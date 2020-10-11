c4713ad548b279da39815a5e5cf9960a3659ae6e
gabrielr12t <gabriel_ps15@hotmail.com>
2020-10-11 17:43:37 -0300

    get servers

--- src/Libraries/DoctorsOffice.Data/Install/FindServers.cs
+++ src/Libraries/DoctorsOffice.Data/Install/FindServers.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 
@@ -11,15 +10,13 @@ namespace DoctorsOffice.Data.Install
         {
             RegistryKey rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server");
             string[] instances = (string[])rk.GetValue("InstalledInstances");
-            if (instances.Length > 0)
+
+            foreach (string element in instances)
             {
-                foreach (string element in instances)
-                {
-                    if (element == "MSSQLSERVER")
-                        yield return Environment.MachineName;
-                    else
-                        yield return Environment.MachineName + @"\" + element;
-                }
+                if (element == "MSSQLSERVER")
+                    yield return Environment.MachineName;
+                else
+                    yield return Environment.MachineName + @"\" + element;
             }
         }
     }
